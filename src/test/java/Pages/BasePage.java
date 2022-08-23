7ae24012c798884e59d762ae4bcba7cceea1c053
llucea <llucea@pentalog.com>
2022-08-23 17:32:01 +0300

    Start Page Object Model

---
+++ src/test/java/Pages/BasePage.java
@@ -0,0 +1,29 @@
+package Pages;
+
+import HelpMethods.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    public WebDriver driver;
+    public AlertMethods alertMethods;
+    public ElementMethods elementMethod;
+    public FrameMethods frameMethod;
+    public PageMethods pageMethod  ;
+    public WindowMethods windowMethod;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        alertMethods = new AlertMethods(driver);
+        elementMethod = new ElementMethods(driver);
+        frameMethod = new FrameMethods(driver);
+        pageMethod = new PageMethods(driver);
+        windowMethod = new WindowMethods(driver);
+    }
+
+
+
+
+}
---
+++ src/test/java/Pages/IndexPage.java
@@ -0,0 +1,21 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class IndexPage extends BasePage {
+
+    public IndexPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "btn2")
+    private WebElement skipsigninElement;
+
+    public void clickSkipSingIn()
+    {
+        elementMethod.clickElement(skipsigninElement);
+    }
+
+}
